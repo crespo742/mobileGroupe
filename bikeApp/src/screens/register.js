cabaee34fa7aa03304e19ce90ea5a4794423a6b3
killian <killian.lecornet@ynov.com>
2023-04-14 09:51:08 +0200

    amélioration de la page register
    des inputs fonctionnel avec une confirmation du mdp

--- bikeApp/src/screens/register.js
+++ bikeApp/src/screens/register.js
@@ -1,26 +1,37 @@
-import {TouchableOpacity, Text} from 'react-native';
+import {TouchableOpacity, Text, TextInput} from 'react-native';
 import React from 'react-native';
 import {getAuth, createUserWithEmailAndPassword} from 'firebase/auth';
 import {firebase} from '@react-native-firebase/auth';
 import { getDatabase, ref, set,get } from "firebase/database";
 import {initializeApp} from 'firebase/app';
 import config from '../config/firebase';
+import { useState } from 'react';
 
 const Register = () => {
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [error, setError] = useState('');
 
   const app = initializeApp(config);
   const auth = getAuth(app);
 
   const test = () => {
-    createUserWithEmailAndPassword(auth, 'ml@mal.com', 'inputs.password')
+    if (password !== confirmPassword) {
+      setError('Les deux mots de passe doivent correspondre');
+      return;
+    }
+
+    createUserWithEmailAndPassword(auth, email, password)
     .then(userCredential => {
       const user = userCredential.user;
       const db = getDatabase();
       
       const userRef = ref(db, 'users/' + user.uid);
       const userData = {
-        email: 'user.email',
-        displayName: 'user.displayName',
+        email: email,
+        password: password,
       };
       return set(userRef, userData);
     })
@@ -35,14 +46,31 @@ const Register = () => {
     });
   }
 
-
-return (
-  <>
-    <TouchableOpacity onPress={test}>
+  return (
+    <>
+      <TextInput
+        placeholder="Email"
+        onChangeText={text => setEmail(text)}
+        value={email}
+      />
+      <TextInput
+        placeholder="Password"
+        secureTextEntry={true}
+        onChangeText={text => setPassword(text)}
+        value={password}
+      />
+      <TextInput
+        placeholder="Confirm Password"
+        secureTextEntry={true}
+        onChangeText={text => setConfirmPassword(text)}
+        value={confirmPassword}
+      />
+      {error !== '' && <Text>{error}</Text>}
+      <TouchableOpacity onPress={test}>
         <Text>Create user</Text>
       </TouchableOpacity>
-  </>
-);
+    </>
+  );
 }
 
 export default Register;
